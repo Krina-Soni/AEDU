649f0bea7c42f68c7491f5964dbd64ad261ad05b
denisha-addweb <denisha.addweb@gmail.com>
2020-02-16 00:41:14 +0530

    Basecase changes

--- src/test/java/AEDU/Testcases/Basecase.java
+++ src/test/java/AEDU/Testcases/Basecase.java
@@ -20,7 +20,7 @@ public class Basecase extends ReportClass {
         System.setProperty("Webdriver.chrome.driver", System.getProperty("user.dir") + "chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-debugging-port=9222");
-        options.addArguments("headless");
+        options.addArguments("--headless");
         options.addArguments("start-maximized"); // open Browser in maximized mode
         options.addArguments("disable-infobars"); // disabling infobars
         options.addArguments("--disable-extensions"); // disabling extensions
