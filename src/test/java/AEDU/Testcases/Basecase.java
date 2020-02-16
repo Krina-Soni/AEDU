3e478ff2e53145ea95b6205952f100864f4619e3
denisha-addweb <denisha.addweb@gmail.com>
2020-02-16 15:11:43 +0530

    hfhfgh

--- src/test/java/AEDU/Testcases/Basecase.java
+++ src/test/java/AEDU/Testcases/Basecase.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import AEDU.Reports.ReportClass;
+
 import java.sql.SQLException;
 
 public class Basecase extends ReportClass {
@@ -19,14 +20,15 @@ public class Basecase extends ReportClass {
     public void initialize() throws SQLException {
         System.setProperty("Webdriver.chrome.driver", System.getProperty("user.dir") + "chromedriver");
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-debugging-port=9222");
-        options.addArguments("--headless");
-        options.addArguments("start-maximized"); // open Browser in maximized mode
-        options.addArguments("disable-infobars"); // disabling infobars
-        options.addArguments("--disable-extensions"); // disabling extensions
-        options.addArguments("--disable-gpu"); // applicable to windows os only
+//        options.addArguments("--remote-debugging-port=9222");
+//        options.addArguments("--headless");
+//        options.addArguments("start-maximized"); // open Browser in maximized mode
+//        options.addArguments("disable-infobars"); // disabling infobars
+//        options.addArguments("--disable-extensions"); // disabling extensions
+//        options.addArguments("--disable-gpu"); // applicable to windows os only
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         options.addArguments("--no-sandbox"); // Bypass OS security model
+        options.setExperimentalOption("useAutomationExtension", false);
 //        System.setProperty("Webdriver.chrome.driver",System.getProperty("user.dir")+"chromedriver");
         driver = new ChromeDriver();
         CommonVar common=new CommonVar();
